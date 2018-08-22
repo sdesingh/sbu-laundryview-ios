eb1ca3e79d04d4738a8adc5907085ae7ee4d47d1
Sam (Macbook) <sqrtmango@gmail.com>
2018-08-22 09:35:22 -0400

    Added tab bar.

---
+++ LaundryView/Controller/Items/TabBar.swift
@@ -0,0 +1,35 @@
+//
+//  TabBar.swift
+//  LaundryView
+//
+//  Created by Sam Singh on 8/22/18.
+//  Copyright © 2018 Mango Hut. All rights reserved.
+//
+
+import UIKit
+
+class TabBar: UIView {
+
+    @IBOutlet var contentView: UIView!
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initializeViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initializeViews()
+    }
+    
+    
+    private func initializeViews(){
+        
+        Bundle.main.loadNibNamed("TabBar", owner: self, options: nil)
+        addSubview(contentView)
+        contentView.frame = self.bounds
+        contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        
+    }
+}
